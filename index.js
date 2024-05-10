c36447f9c688e19f7141b8b4da611833ac9ac618
marco <marco.spallone@student.unibz.it>
2024-05-10 17:22:29 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mysql = require('mysql');
+
+const app = express();
+
+const connection = mysql.createConnection({
+    host: 'be.cdjkupklvmzr.us-east-1.rds.amazonaws.com',
+    user: 'admin',
+    password: 'password',
+    database: 'be'
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.error('Errore di connessione al database:', err);
+    } else {
+        console.log('Connessione al database riuscita.');
+    }
+});
+
+// Definisci le route e i controller qui...
+
+const PORT = process.env.DB_PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server in ascolto sulla porta ${PORT}`);
+});
