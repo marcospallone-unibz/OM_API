d505f4de8f25a4999653a0a13a5320c47ab2970c
marco <marco.spallone@student.unibz.it>
2024-05-13 18:56:30 +0200

    db connection

--- index.js
+++ index.js
@@ -3,27 +3,27 @@ const mysql = require('mysql');
 
 const app = express();
 
-// const connection = mysql.createConnection({
-//     host: 'be.cdjkupklvmzr.us-east-1.rds.amazonaws.com',
-//     user: 'admin',
-//     password: 'password',
-//     database: 'be',
-//     port:3306
-// });
+const connection = mysql.createConnection({
+  host: 'be.cdjkupklvmzr.us-east-1.rds.amazonaws.com',
+  user: 'admin',
+  password: 'password',
+  database: 'be',
+  port: 3306
+});
 
-// connection.connect((err) => {
-//     if (err) {
-//         console.error('Errore di connessione al database:', err);
-//     } else {
-//         console.log('Connessione al database riuscita.');
-//     }
-// });
+connection.connect((err) => {
+  if (err) {
+    console.error('Errore di connessione al database:', err);
+  } else {
+    console.log('Connessione al database riuscita.');
+  }
+});
 
 app.get('/', (req, res) => {
-    res.send('Hello world!');
-  });
+  res.send('Hello world!');
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server in ascolto sulla porta ${PORT}`);
+  console.log(`Server in ascolto sulla porta ${PORT}`);
 });
