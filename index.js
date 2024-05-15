300cec137e00dac32d10222daf63385836cd7438
marco <marco.spallone@student.unibz.it>
2024-05-15 19:06:02 +0200

    test conn

--- index.js
+++ index.js
@@ -4,24 +4,24 @@ const cors = require('cors');
 
 const app = express();
 
-const connection = mysql.createConnection({
-  host: 'be.cdjkupklvmzr.us-east-1.rds.amazonaws.com',
-  user: 'admin',
-  password: 'password',
-  database: 'be',
-  port: 3306
-});
+// const connection = mysql.createConnection({
+//   host: 'be.cdjkupklvmzr.us-east-1.rds.amazonaws.com',
+//   user: 'admin',
+//   password: 'password',
+//   database: 'be',
+//   port: 3306
+// });
 
-connection.connect((err) => {
-  if (err) {
-    console.error('Errore di connessione al database:', err);
-  } else {
-    console.log('Connessione al database riuscita.');
-  }
-});
+// connection.connect((err) => {
+//   if (err) {
+//     console.error('Errore di connessione al database:', err);
+//   } else {
+//     console.log('Connessione al database riuscita.');
+//   }
+// });
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://52.91.184.190:3000');
+  res.setHeader('Access-Control-Allow-Origin', 'https://52.91.184.190:3000');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
