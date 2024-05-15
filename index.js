72b46c2ceaaffdacf1d6735aaf55d766918a2c71
marco <marco.spallone@student.unibz.it>
2024-05-15 18:45:29 +0200

    cors

--- index.js
+++ index.js
@@ -20,7 +20,12 @@ connection.connect((err) => {
   }
 });
 
-app.use(cors())
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://52.91.184.190:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 app.get('/', (req, res) => {
   res.send('Hello world!');
