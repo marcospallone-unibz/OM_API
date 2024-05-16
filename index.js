1c81c2f1225da529cc790446bec92198a3f2af95
marco <marco.spallone@student.unibz.it>
2024-05-16 23:17:02 +0200

    cors

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const mysql = require('mysql');
+const cors = require ('cors');
 
 const app = express();
+
+app.use(cors());
 
 // const connection = mysql.createConnection({
 //   host: 'be.cdjkupklvmzr.us-east-1.rds.amazonaws.com',
