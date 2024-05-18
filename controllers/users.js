4779dee8e9d5a241f1284e02b8dc579ac68ebfac
marco <marco.spallone@student.unibz.it>
2024-05-18 16:43:11 +0200

    be

--- controllers/users.js
+++ controllers/users.js
@@ -7,7 +7,7 @@ function allUsers(connection, req, res) {
       return res.status(500).json({ error: 'Errore' });
     } else if (results.length > 0) {
       console.log(results)
-      res.json({ message: 'Get all users successful!' });
+      res.json({ message: 'Get all users successful!' + results.toString() });
     } else {
       console.log('Errore nella richiesta (?)')
       res.json({ message: 'Errore nella richiesta (?)' });
